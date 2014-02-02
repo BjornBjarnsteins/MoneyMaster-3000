23a56c75192035460a36bbf47bb40a8b19763845
BjornBjarnsteins <bjb46@hi.is>
2014-02-02 17:56:09 +0000

    Lagaði store og load föllin. Ætti að geyma gögnin rétt

--- storage.py
+++ storage.py
@@ -128,10 +128,10 @@ def resetFile(filename):
 
 # Notkun: storeLoan(l)
 # Fyrir:  l er lán
-# Eftir:  l hefur verið geymt í skránni 'loans.txt' á forminu 'name-amount-interest-pay', eitt lán í línu
+# Eftir:  l hefur verið geymt í skránni 'loans.txt' á forminu 'name-amount-interest-months-index', eitt lán í línu
 def storeLoan(l):
 	storage = open('loans.txt', 'a')
-	storage.write('%s-%d-%f-%d\n' % (l.name, l.amount, l.interest, l.pay)) # þarf að breyta, þurfum ekki pay, þurfum í staðin tíma sem er eftir af láninu og hvort hann sé verðtryggður.
+	storage.write('%s-%d-%f-%d-%s\n' % (l.name, l.amount, l.interest, l.m, l.dex)) 
 
 # Notkun: storeAllLoans(l)
 # Fyrir:  l er listi af lánum
@@ -148,7 +148,7 @@ def loadLoans():
 	loans = []
 	for line in open('loans.txt'):
 		args = line.split('-')
-		loans = loans + [Loan(args[0], int(args[1]), float(args[2]), int(args[3])]
+		loans = loans + [Loan.Loan(args[0], int(args[1]), float(args[2]), int(args[3]), args[4]==1)]
 	return loans
 
 
@@ -159,7 +159,7 @@ def loadLoans():
 # Eftir:  s er geymdur í 'savings.txt' á forminu name-amount-interest-pay, einn reikningur í línu
 def storeSAcct(s):
 	storage = open('savings.txt', 'a')
-	storage.write('%s-%d-%f-%d\n' % (s.name, s.amount, s.interest, s.pay))
+	storage.write('%s-%d-%f-%d-%s-%s\n' % (s.name, s.amount, s.interest, s.index, s.bound))
 
 # Notkun: storeAllSAccts(s)
 # Fyrir:  s er listi af reikningum
@@ -175,13 +175,14 @@ def loadSAccts():
 	SAccts = []
 	for line in open('savings.txt'):
 		args = line.split('-')
-		SAccts = SAccts + [SavingAcct(args[0], int(args[1]), float(args[2]), int(args[3]))]
+		SAccts = SAccts + [Savings.Savings(args[0], int(args[1]), float(args[2]), int(args[3]), args[4]==1, args[5]==1)]
 	return SAccts
 
 if __name__=="__main__":
-	l1 = Loan('bílalán', 100000, 0.05)
-	l2 = Loan('húsnæðislán', 10000000, 0.04)
+	l1 = Loan.Loan('bílalán', 100000, 0.05, 5, 1)
+	l2 = Loan.Loan('húsnæðislán', 10000000, 0.04, 10, 0)
 	loans = [l1, l2]
 	storeAllLoans(loans)
 	loans2 = loadLoans()
 	print(loans2[0])
+	print(loans2[1])
