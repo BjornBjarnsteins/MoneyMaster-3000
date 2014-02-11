7e5cf10728215a8c0fc0f62fc2aec9db9efd71d0
andreabjork <andreabjork43@gmail.com>
2014-02-11 19:45:20 +0000

    uppfært - plotLS nánast tilbúið

--- plot.py
+++ plot.py
@@ -21,18 +21,29 @@ def plotPayProg(monthly, m):
 #       Mynd2: Höfuðstóll láns og inneign sparnaðar sem fall af tíma.
 #               Miðast við að greitt sé aukalega upphæð monthly inn a sparnað i m mánuði.
 def plotLS(L,S,monthly,m):
+
+    fig1 = figure(1)
+    p1 = fig1.add_subplot(111, autoscale_on=True)
+    fig2 = figure(2)
+    p2 = fig2.add_subplot(111, autoscale_on=True)
+    
     #Case1: Borgum niður lán:
     Lx = L.datLoanDebt(monthly, m)
     n = len(L.payProgression(monthly,m))
     N = max(m-n-1,0)
     Sx = S.datSavings2(L.baseFee, monthly, n+1, N, L.m)
 
-    plot(Lx[0],Lx[1])
-    plot(Sx[0],Sx[1])
-
+    p1.plot(Lx[0],Lx[1])
+    p1.plot(Sx[0],Sx[1])
+    
+    #Case2: Borgum inn a sparnaðarreikning.
+    Lx = L.datLoanDebt(0,m)
+    n = len(L.payProgression(0,m))
+    Sx = S.datSavings(monthly, m, n)
+    p2.plot(Lx[0],Lx[1])
+    p2.plot(Sx[0],Sx[1])
     show()
     
-    return
     #Hjálparföll: datDebtProg
     #             datSavings (Ath eftir að útfæra betur!!)
 
